def1c11663f17ce6ecd676d9cf8646007ea9d0b9
Kishan Kumar <myselfkeke@gmail.com>
2025-12-05 19:22:15 +0530

    each components of App.jsx is moved to its seprate file

---
+++ chatbot-project/src/Components/ChatInput.jsx
@@ -0,0 +1,72 @@
+import { useState } from "react";
+import { Chatbot } from "supersimpledev";
+import loadingSpinner from "../../src/assets/loading-spinner.gif";
+
+export function ChatInput({ chatMessages, setChatMessages }) {
+  const [inputText, setInputText] = useState("");
+
+  function saveInputText(e) {
+    setInputText(e.target.value);
+  }
+  async function sendMessage() {
+    setInputText(""); // to immidiatly remove message when uset hit enter or click send
+    const newChatMessages = [
+      ...chatMessages,
+      {
+        message: inputText,
+        sender: "user",
+        id: crypto.randomUUID(),
+      },
+    ];
+
+    setChatMessages(newChatMessages);
+    setChatMessages([
+      ...newChatMessages,
+      // This creates a temporaprofry Loading... message.
+      // Because we don't save this message in newChatMessages,
+      // it will be remove later, when we add the response.
+      {
+        message: (
+          <img className="loading-spinner" src={loadingSpinner} alt="" />
+        ),
+        sender: "robot",
+        id: crypto.randomUUID(),
+      },
+    ]);
+
+    const response = await Chatbot.getResponseAsync(inputText);
+    setChatMessages([
+      ...newChatMessages,
+      {
+        message: response,
+        sender: "robot",
+        id: crypto.randomUUID(),
+      },
+    ]);
+
+    setInputText("");
+  }
+  function handleKeyDown(e) {
+    if (e.key === "Enter") {
+      sendMessage();
+    } else if (e.key === "Escape") {
+      setInputText("");
+    }
+  }
+
+  return (
+    <div className="chat-input-container">
+      <input
+        className="chat-input"
+        placeholder="Send a message to Chatbot"
+        size="30"
+        onChange={saveInputText}
+        onKeyDown={handleKeyDown}
+        value={inputText}
+      />
+      <button className="send-button" onClick={sendMessage}>
+        Send
+      </button>
+    </div>
+  );
+}
---
+++ chatbot-project/src/Components/ChatMessage.jsx
@@ -0,0 +1,26 @@
+import robotProfileImage from "../../src/assets/robot.png";
+import userProfileImage from "../../src/assets/user.png";
+
+export function ChatMessage({ message, sender }) {
+  return (
+    <div
+      className={sender === "user" ? "chat-message-user" : "chat-message-robot"}
+    >
+      {sender === "robot" && (
+        <img
+          src={robotProfileImage}
+          className="profile-image"
+          alt="Robot Profile image"
+        />
+      )}
+      <div className="chat-message-text">{message}</div>
+      {sender === "user" && (
+        <img
+          src={userProfileImage}
+          className="profile-image"
+          alt="User Profile image"
+        />
+      )}
+    </div>
+  );
+}
